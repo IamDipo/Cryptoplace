d95f6b5abfab821001d35e250c8c04ac5bac8b84
Fatogun Oladipupo <fatogunoladipupo@gmail.com>
2026-01-02 19:16:52 +0100

    new commit

--- src/components/LineChart/LineChart.jsx
+++ src/components/LineChart/LineChart.jsx
@@ -6,44 +6,19 @@ const LineChart = ({ historicalData }) => {
   const [data, setData] = useState([["Date", "Prices"]]);
 
   useEffect(() => {
-    if (!historicalData?.prices) return;
-
-    const dataCopy = [
-      ["Date", "Prices"],
-      ...historicalData.prices.map((item) => [
-        new Date(item[0]), 
-        item[1],
-      ]),
-    ];
-
-    setData(dataCopy);
+    let dataCopy = [["Date", "Prices"]];
+    if (historicalData.prices) {
+      historicalData.prices.map((item) => {
+        dataCopy.push([
+          `${new Date(item[0]).toLocaleDateString().slice(0, -5)}`,
+          item[1],
+        ]);
+      });
+      setData(dataCopy);
+    }
   }, [historicalData]);
 
-  return (
-    <Chart
-      chartType="LineChart"
-      width="100%"
-      height="100%"
-      data={data}
-      options={{
-        hAxis: {
-          format: "MMM d",
-          slantedText: false,
-          maxTextLines: 1,
-          showTextEvery: 2,
-        },
-        vAxis: {
-          gridlines: { color: "#eee" },
-        },
-        legend: "none",
-        chartArea: {
-          width: "85%",
-          height: "70%",
-        },
-      }}
-    />
-  );
+  return <Chart chartType="LineChart" height="100%" legendToggle data={data} />;
 };
 
 export default LineChart;
-
